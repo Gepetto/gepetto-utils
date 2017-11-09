364ec6190981a9c508b27f2c5d72847970566490
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-11-09 15:39:58 +0100

    startapp

---
+++ gepetto_packages/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GepettoPackagesConfig(AppConfig):
+    name = 'gepetto_packages'
--- gepkg/settings.py
+++ gepkg/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gepetto_packages',
 ]
 
 MIDDLEWARE = [
