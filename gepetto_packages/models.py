bb6f469d912008001463a37bcd3bb37dfe7d4b05
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-11-09 16:55:35 +0100

    django 1.11, models, migrations

--- gepetto_packages/models.py
+++ gepetto_packages/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from ndh.models import TimeStampedModel, NamedModel
+
+
+class Project(NamedModel):
+    pass
+
+
+class License(NamedModel):
+    pass
+
+
+class Package(NamedModel, TimeStampedModel):
+    project = models.ForeignKey(Project)
+    url = models.URLField(max_length=200, blank=True, null=True)
+    license = models.ForeignKey(License, blank=True, null=True)
+
+
+class Repo(TimeStampedModel):
+    url = models.URLField(max_length=200, unique=True)
+    package = models.ForeignKey(Package)
+    default_branch = models.CharField(max_length=50)
+    open_issues = models.PositiveSmallIntegerField(blank=True, null=True)
+    open_pr = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.url
