dab80451d92210dd55c419a579d7cf1553cab127
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-03-06 14:10:05 +0100

    delete dashboard

--- dashboard/gepetto_packages/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.admin import site
-
-from .models import License, Package, Project, Repo
-
-for model in [License, Package, Project, Repo]:
-    site.register(model)
--- dashboard/gepetto_packages/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GepettoPackagesConfig(AppConfig):
-    name = 'gepetto_packages'
