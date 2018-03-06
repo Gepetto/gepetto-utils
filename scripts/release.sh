7e0496f18c189c6216928e461d8241f7e3183037
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-03-06 13:26:30 +0100

    improve release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,16 +3,24 @@
 set -x
 set -e
 
-[[ -x cmake/git-archive-all.sh ]]
 
 TAG=$1
 SOFT=${2:-$(basename $(pwd))}
+SOFTAG="${SOFT}-${TAG}"
 
-echo Releasing $SOFT $TAG
+echo Releasing $SOFTAG
 
 rm -vf *.tar* /tmp/*.tar*
-git tag -u $KEY -s "v$TAG" -m "Release v$TAG"
-./cmake/git-archive-all.sh --prefix "${SOFT}-${TAG}/" -v "${SOFT}-${TAG}.tar"
-gzip "${SOFT}-${TAG}.tar"
-gpg --armor --detach-sign "${SOFT}-${TAG}.tar.gz"
-git push --tags
+git tag -s "v$TAG" -m "Release v$TAG"
+if [[ -d cmake && -x cmake/git-archive-all.sh ]]
+then
+    ./cmake/git-archive-all.sh --prefix "${SOFTAG}/" -v "${SOFTAG}.tar"
+    gzip "${SOFTAG}.tar"
+else
+    git archive --format=tar.gz --prefix="${SOFTAG}/" HEAD > ${SOFTAG}.tar.gz
+fi
+gpg --armor --detach-sign "${SOFTAG}.tar.gz"
+
+echo -e "git push --tags
+git log --pretty=oneline $(git tag -l|tail -n2|sed ':a;N;$!ba;s/\n/../g') | sed 's/.\{48\}/*/'
+# Draft new release"
