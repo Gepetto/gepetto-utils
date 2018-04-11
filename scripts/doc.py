03e39ef845ef4055d4133af827d1315853ec71b0
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-04-11 19:10:12 +0200

    détails

--- scripts/doc.py
+++ scripts/doc.py
@@ -2,7 +2,7 @@
 
 import os
 from pathlib import Path
-from zipfile import ZipFile
+from zipfile import BadZipFile, ZipFile
 from io import BytesIO
 import requests
 
@@ -14,12 +14,12 @@ VERSION = '16.04'
 
 if __name__ == '__main__':
     for project, namespace, branch in requests.get(f'{RAINBOARD}/doc').json()['ret']:
-        r = requests.get(f'{GITLAB}/{namespace}/{project}/-/jobs/artifacts/{branch}/download',
-                         {'job': f'robotpkg-{project}-{VERSION}'}, stream=True)
+        url = f'{GITLAB}/{namespace}/{project}/-/jobs/artifacts/{branch}/download'
+        r = requests.get(url, {'job': f'robotpkg-{project}-{VERSION}'}, stream=True)
         try:
             z = ZipFile(BytesIO(r.content))
             path = DOC / namespace / project / branch
             path.mkdir(parents=True, exist_ok=True)
             z.extractall(str(path))
-        except:
+        except BadZipFile:
             continue
