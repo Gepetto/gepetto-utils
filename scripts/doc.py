dd95a061ade6de7ec25b664697de76a772e953e4
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-04-19 14:09:36 +0200

    doc.py: generates an index

--- scripts/doc.py
+++ scripts/doc.py
@@ -10,16 +10,30 @@ DOC = Path('/net/pongo/vol/vol_projects/partage_gepetto/Doc')
 GITLAB = 'https://gepgitlab.laas.fr'
 RAINBOARD = 'http://rainboard.laas.fr'
 VERSION = '16.04'
+INDEX = DOC / 'index.html'
+HEAD = DOC / 'index.head.html'
 
 
 if __name__ == '__main__':
-    for project, namespace, branch in requests.get(f'{RAINBOARD}/doc').json()['ret']:
+    with INDEX.open('w') as f:
+        with HEAD.open() as head:
+            f.write(head.read())
+
+    for project, namespace, branch in sorted(requests.get(f'{RAINBOARD}/doc').json()['ret']):
         url = f'{GITLAB}/{namespace}/{project}/-/jobs/artifacts/{branch}/download'
+        path = DOC / namespace / project / branch
         r = requests.get(url, {'job': f'robotpkg-{project}-{VERSION}'}, stream=True)
         try:
             z = ZipFile(BytesIO(r.content))
-            path = DOC / namespace / project / branch
             path.mkdir(parents=True, exist_ok=True)
             z.extractall(str(path))
         except BadZipFile:
-            continue
+            pass
+
+        if path.exists():
+            with INDEX.open('a') as f:
+                link = path.relative_to(DOC)
+                print(f'<tr><td>{project}</td><td>{namespace}</td><td><a href="{link}">{branch}</a></td></tr>', file=f)
+
+    with INDEX.open('a') as f:
+        print('</table></body></html>', file=f)
