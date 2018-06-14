8f28dfa5133b0892f602f37bb36c9067e5701c31
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-06-14 15:59:01 +0200

    axis_record: line breaks

--- scripts/axis_record.sh
+++ scripts/axis_record.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ffmpeg -i http://axis-ptz1/mjpg/video.mjpg -c:v libx265 axis-ptz1-$(date +'%Y-%m-%d-%A-%H:%M:%S').mp4
+ffmpeg \
+    -i http://axis-ptz1/mjpg/video.mjpg \
+    -c:v libx265 \
+    axis-ptz1-$(date +'%Y-%m-%d-%A-%H:%M:%S').mp4
