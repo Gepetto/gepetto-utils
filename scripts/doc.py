f97c236e22f977566be2cd2cab251245913d4b8c
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-11-19 15:56:35 +0100

    doc.py: allow py < 3.6

--- scripts/doc.py
+++ scripts/doc.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import os
+from io import BytesIO
 from pathlib import Path
 from zipfile import BadZipFile, ZipFile
-from io import BytesIO
+
 import requests
 
 DOC = Path('/net/pongo/vol/vol_projects/partage_gepetto/Doc')
@@ -12,14 +12,13 @@ RAINBOARD = 'http://rainboard.laas.fr'
 INDEX = DOC / 'index.html'
 HEAD = DOC / 'index.head.html'
 
-
 if __name__ == '__main__':
     with INDEX.open('w') as f:
         with HEAD.open() as head:
             f.write(head.read())
 
-    for project, namespace, branch in sorted(requests.get(f'{RAINBOARD}/doc').json()['ret']):
-        url = f'{GITLAB}/{namespace}/{project}/-/jobs/artifacts/{branch}/download'
+    for project, namespace, branch in sorted(requests.get('%s/doc' % RAINBOARD).json()['ret']):
+        url = '%s/%s/%s/-/jobs/artifacts/%s/download' % (GITLAB, namespace, project, branch)
         path = DOC / namespace / project / branch
         r = requests.get(url, {'job': 'doc-coverage'}, stream=True)
         try:
@@ -33,12 +32,12 @@ if __name__ == '__main__':
             with INDEX.open('a') as f:
                 link = path.relative_to(DOC)
                 doxygen, coverage = link / 'doxygen-html', link / 'coverage'
-                print(f'<tr><td>{project}</td><td>{namespace}</td><td>{branch}</td><td>', file=f)
+                print('<tr><td>%s</td><td>%s</td><td>%s</td><td>' % (project, namespace, branch), file=f)
                 if (DOC / doxygen).is_dir():
-                    print(f'<a href="{doxygen}">Doc</a>', file=f)
+                    print('<a href="%s">Doc</a>' % doxygen, file=f)
                 print('</td><td>', file=f)
                 if (DOC / coverage).is_dir():
-                    print(f'<a href="{coverage}">Coverage</a>', file=f)
+                    print('<a href="%s">Coverage</a>' % coverage, file=f)
                 print('</td></tr>', file=f)
 
     with INDEX.open('a') as f:
