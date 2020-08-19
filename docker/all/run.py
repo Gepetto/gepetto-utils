3268895f2ebb6a3591868e62c5b85c0a4d3801d1
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-08-19 12:57:51 +0200

    all: pinocchio

--- docker/all/run.py
+++ docker/all/run.py
@@ -2,6 +2,8 @@
 
 import eigenpy
 import hppfcl
+import pinocchio
 
 print(eigenpy.Quaternion(1, 2, 3, 4).norm())
 print(hppfcl.Capsule(2, 3).computeVolume())
+print(pinocchio.SE3.Identity().inverse())
