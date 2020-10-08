bd24b00ab9c1fd7180d7bbddc4a5141a41f796bd
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-10-08 11:54:14 +0200

    manylinux: boost 1.74

--- docker/manylinux2014/build/install_boost.sh
+++ docker/manylinux2014/build/install_boost.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eux
 
 yum -y install wget python-devel vim
-wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.bz2
-tar --bzip2 -xf boost_1_72_0.tar.bz2
-cd boost_1_72_0
+wget https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.bz2
+tar --bzip2 -xf boost_1_74_0.tar.bz2
+cd boost_1_74_0
 ./bootstrap.sh --prefix=/usr/local
 cp ../project-config.jam .
 ./b2 install link=shared python=2.7,3.5,3.6,3.7,3.8 -j"$(nproc)"
