77c9631a21142452fb7eecebf201a5c47bdaa5c6
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-10-08 10:27:08 +0200

    align

--- docker/all/run.py
+++ docker/all/run.py
@@ -12,7 +12,7 @@ with open('/dist') as f:
 
 if '20.04' in dist:
     print('*' * 74)
-print(sys.version.split()[0])
+print('{: <6s}'.format(sys.version.split()[0]))
 print(eigenpy.Quaternion(1, 2, 3, 4).norm())
 print(hppfcl.Capsule(2, 3).computeVolume())
 print(pinocchio.SE3.Identity().inverse())
