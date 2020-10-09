d36d8ef1de0fc863d81c37f87dc296c117fc26f0
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-10-10 00:43:16 +0200

    Revert "manylinux build only 3.7 for tests"
    This reverts commit b665150ee05d33fe8a2509cab72d84dfb12e54d3.

--- docker/manylinux2014/all.sh
+++ docker/manylinux2014/all.sh
@@ -5,7 +5,7 @@ docker run -v "$(pwd -P):/io" --rm -t manylinux rm -rf /io/dist /io/wheelhouse
 docker run -v "$(pwd -P):/io" --rm -t manylinux /scripts/setup.sh eigenpy
 docker run -v "$(pwd -P):/io" --rm -t manylinux /scripts/setup.sh hpp-fcl
 docker run -v "$(pwd -P):/io" --rm -t manylinux /scripts/setup.sh pinocchio
-for PYVER in 2.7 3.5 3.6 3.7 3.8
+for PYVER in 2.7 3.5 3.6 3.7 3.8 3.9
 do
     docker build -f test.Dockerfile --build-arg PYVER=$PYVER -t manylinux-test:$PYVER .
     docker run --rm -t manylinux-test:$PYVER
--- docker/manylinux2014/build/setup_python.sh
+++ docker/manylinux2014/build/setup_python.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -eux
 
-rm /opt/python/{cp35-cp35m,cp36-cp36m,cp38-cp38,cp39-cp39}
-
 # Install cmake, ninja and wheel
-PY37_BIN=/opt/python/cp37-cp37m/bin
-$PY37_BIN/pip install cmake ninja wheel
-ln -s $PY37_BIN/cmake /usr/bin/
-ln -s $PY37_BIN/ninja /usr/bin/
-ln -s $PY37_BIN/wheel /usr/bin/
+PY39_BIN=/opt/python/cp39-cp39/bin
+$PY39_BIN/pip install cmake ninja wheel
+ln -s $PY39_BIN/cmake /usr/bin/
+ln -s $PY39_BIN/ninja /usr/bin/
+ln -s $PY39_BIN/wheel /usr/bin/
+
+# Setup python 2.7
+yum -y install python-pip
+PY27_BIN=/opt/python/cp27-cp27mu/bin
+mkdir -p $PY27_BIN
+ln -s /usr/bin/python $PY27_BIN/
+ln -s /usr/bin/pip $PY27_BIN/
+touch $PY27_BIN/python2.7
+pip2 install --upgrade pip
+pip2 install scikit-build --user
 
 # Upgrade pip and install scikit-build
 for PYBIN in /opt/python/*/bin; do
