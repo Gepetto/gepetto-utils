b7cef2403580e9f98052ba30e8caf57faf53304c
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-10-09 15:33:43 +0200

    all: curves & multiconact-api

--- docker/all/run.py
+++ docker/all/run.py
@@ -2,16 +2,18 @@
 
 import sys
 
-import eigenpy
-import hppfcl
-import pinocchio
-import example_robot_data
-import tsid
 import crocoddyl
+import curves
+import eigenpy
+import example_robot_data
+import hppfcl
+import multicontact_api
+import numpy as np
+import pinocchio
+import tsid
 
 with open('/dist') as f:
     dist = f.read()
-
 
 if '20.04' in dist:
     print('*' * 74)
@@ -24,3 +26,5 @@ URDF = "/talos_data/robots/talos_left_arm.urdf"
 PATH = example_robot_data.robots_loader.getModelPath(URDF)
 print(tsid.RobotWrapper(PATH + URDF, [PATH], False).na)
 print(crocoddyl.ActionModelUnicycle().nr)
+print(curves.bezier(np.array([[1, 2, 3], [4, 5, 6], [4, 5, 6], [4, 5, 6], [4, 5, 6]]), 0.2, 1.5).dim())
+print(multicontact_api.ContactModel().mu)
