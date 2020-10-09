b665150ee05d33fe8a2509cab72d84dfb12e54d3
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-10-09 17:16:48 +0200

    manylinux build only 3.7 for tests

--- docker/manylinux2014/build/install_boost.sh
+++ docker/manylinux2014/build/install_boost.sh
@@ -6,5 +6,5 @@ tar --bzip2 -xf boost_1_74_0.tar.bz2
 cd boost_1_74_0
 ./bootstrap.sh --prefix=/usr/local
 cp ../project-config.jam .
-./b2 install link=shared python=2.7,3.5,3.6,3.7,3.8,3.9 -j"$(nproc)"
+./b2 install link=shared python=3.7 -j"$(nproc)"
 rm -rf /build
--- docker/manylinux2014/build/setup_python.sh
+++ docker/manylinux2014/build/setup_python.sh
@@ -1,21 +1,13 @@
 #!/bin/bash -eux
 
-# Install cmake, ninja and wheel
-PY39_BIN=/opt/python/cp39-cp39/bin
-$PY39_BIN/pip install cmake ninja wheel
-ln -s $PY39_BIN/cmake /usr/bin/
-ln -s $PY39_BIN/ninja /usr/bin/
-ln -s $PY39_BIN/wheel /usr/bin/
+rm /opt/python/{cp35-cp35m,cp36-cp36m,cp38-cp38,cp39-cp39}
 
-# Setup python 2.7
-yum -y install python-pip
-PY27_BIN=/opt/python/cp27-cp27mu/bin
-mkdir -p $PY27_BIN
-ln -s /usr/bin/python $PY27_BIN/
-ln -s /usr/bin/pip $PY27_BIN/
-touch $PY27_BIN/python2.7
-pip2 install --upgrade pip
-pip2 install scikit-build --user
+# Install cmake, ninja and wheel
+PY37_BIN=/opt/python/cp37-cp37m/bin
+$PY37_BIN/pip install cmake ninja wheel
+ln -s $PY37_BIN/cmake /usr/bin/
+ln -s $PY37_BIN/ninja /usr/bin/
+ln -s $PY37_BIN/wheel /usr/bin/
 
 # Upgrade pip and install scikit-build
 for PYBIN in /opt/python/*/bin; do
